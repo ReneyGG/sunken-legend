6d37fe050b6a1bc349ec81e94869dd2a3f22a5fd
ReneyGG <jakubczyzycki12@gmail.com>
2024-04-02 19:18:51 +0200

    fin

---
+++ scenes/Fin/fin.gd
@@ -0,0 +1,14 @@
+extends Control
+
+@export var next : PackedScene
+
+var pressed := false
+
+func _ready():
+	$AnimationPlayer.play("fin")
+
+func _on_texture_button_pressed():
+	if pressed:
+		return
+	pressed = true
+	Transition.change_scene(next)
--- scenes/Intro/intro.gd
+++ scenes/Intro/intro.gd
@@ -1,14 +1,18 @@
 extends Control
 
 var clicks := 0
+var next := false
 
 func _ready():
+	$Label.text = "An old, sealed book has been found."
+	$Label2.text = "A testament to Atlantis."
 	$AnimationPlayer.play("intro")
 
 func _on_book_button_pressed():
-	if clicks == 8:
+	if next:
+		get_tree().change_scene_to_file("res://scenes/Intro/choice.tscn")
+	if clicks >= 8:
 		$Open.play()
-	elif clicks > 8:
 		$BookButton.disabled = true
 		intro_2()
 		return
@@ -19,8 +23,9 @@ func _on_book_button_pressed():
 	$BookButton/AnimationPlayer.play("pressed")
 
 func intro_2():
+	next = true
 	$Label.visible_ratio = 0
 	$Label2.visible_ratio = 0
-	$Label.text = ""
-	$Label2.text = ""
+	$Label.text = "Written in graceful, dainty runes."
+	$Label2.text = " Fate of the lost city."
 	$AnimationPlayer.play("intro_2")
